0b9b530f67e8b6b61a942ddcb2a0a781b7e466b0
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-03-31 23:07:00 -0300

    Iniciando con sessiones con Redis

--- db/schema.js
+++ db/schema.js
@@ -58,11 +58,11 @@ var User = mongoose.model('User', UserSchema);
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
 mongoose.connect(uristring, function (err, res) {
-  if (err) {
-  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-  console.log ('Succeeded connected to: ' + uristring);
-  }
+	if (err) {
+		console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+	} else {
+		console.log ('Succeeded connected to: ' + uristring);
+	}
 });
 
 module.exports = mongoose;
