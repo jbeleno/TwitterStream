3a8b405a80c71b8437df61255e7970d67bed1b2e
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-04-01 23:25:29 -0300

    Redis sessions working perfectly

--- config/sessions.js
+++ config/sessions.js
@@ -6,36 +6,16 @@
 * This file handles with all the schema in the redis database, it's the base
 * and the central file to use all the sessions in this website. 
 **/
-var redis = require('redis');
+
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 
 var hour = 3600000;
 
-var redisClient = redis.createClient({
-	host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD
-});
-
-redisClient.on('error', function(err) {
-     console.log('Redis error: ' + err);
-});
-
-redisClient.on('warning', function(err) {
-     console.log('Redis warning: ' + err);
-});
-
-redisClient.on('connect', function() {
-     console.log('Redis connected');
-});
-
-redisClient.on('ready', function(data) {
-     console.log('Redis ready');
-});
-
 var SessionObject = new RedisStore({
-    client: redisClient
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    pass: process.env.REDIS_PASSWORD
 });
 
 module.exports = function(){
--- routes/users.js
+++ routes/users.js
@@ -57,9 +57,7 @@ router.post('/login', function(req, res, next) {
 		  		if(user){
 		  			// Saving the session id, the save function is just necessary
 		  			// when the request type is different from GET
-		  			console.log("BEFORE: "+req.session.idUser + JSON.stringify(user));
 		  			req.session.idUser = user._id;
-		  			console.log("AFTER: "+req.session.idUser + JSON.stringify(user));
 	      			req.session.save(function(err) {});
 
 		  			res.json({status: "OK"});
